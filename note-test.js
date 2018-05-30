5893f0677faa9d692dfd9d0fb2946287f6d02481
Dione Estabillo <dione.estabillo@gmail.com>
2018-05-30 15:10:09 +0100

    Added assert.js

--- note-test.js
+++ note-test.js
@@ -1,21 +1,14 @@
-(function(exports) {
-  function testNoteDefaultText(){
-    var note = new Note("My favourite language is Javascript");
-    if (note.text !== "My favourite language is Javascript") {
-      throw new Error("Note doesn't have a default text")
-    }
-  };
+function testNoteDefaultText(){
+  var note = new Note("My favourite language is Javascript");
+  assert.isTrue(note.text === "My favourite language is Javascript");
+};
 
-  testNoteDefaultText();
-})(this);
+testNoteDefaultText();
 
-(function(exports) {
-  function testShowText(){
-    var note = new Note("Hello");
-    if (note.showText() !== "Hello") {
-      throw new Error("Note doesn't have a text property")
-    }
-  };
 
-  testShowText();
-})(this);
+function testshowText(){
+  var note = new Note("Hello");
+  assert.isTrue(note.text === "Hello");
+};
+
+testshowText();
